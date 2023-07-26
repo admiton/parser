98d6eb9b706b7b1dc619232408efe1ae99313931
mirceamaierean <mircea.maierean@yahoo.com>
2023-07-26 16:30:38 +0200

    classes

--- admito_parser.py
+++ admito_parser.py
@@ -3,164 +3,161 @@ import re
 import json
 
 
-def adjust_answer(line):
-    # if the line starts with digits, followed by period, we remove the digits
-    if re.match(r"\d+\.", line):
-        line = re.sub(r"\d+\. ", "", line)
+class PdfParser:
+    def __init__(self):
+        self.can_strip = 1
 
-    # if the line contains points, we remove them, and also the digits
-    if re.search(r"\d\.?\d* points", line):
-        line = re.sub(r"\d*\.?\d* points", "", line)
+    def adjust_answer(self, line):
+        # if the line starts with digits, followed by period, we remove the digits
+        if re.match(r"\d+\.", line):
+            line = re.sub(r"\d+\. ", "", line)
 
-    line = line.strip()
+        # if the line contains points, we remove them, and also the digits
+        if re.search(r"\d\.?\d* points", line):
+            line = re.sub(r"\d*\.?\d* points", "", line)
 
-    return line
+        line = line.strip()
 
+        return line
 
-can_strip = 1
+    def adjust_question(self, line):
+        # arrange the line, so each answer is on a new line
+        if re.search(r"[A-D]\.", line):
+            line = re.sub(r"([A-D]\.)( )?", r"\n\1\n", line)
+        # we want to have clearly the algorithm separated between the Algorithm and EndAlg
+        if re.search(r"EndAlg", line):
+            line = re.sub(r"(\d+\. *)?(EndAlg\w*)( *)(.*$)", r"\2\n\n\4", line)
+            self.can_strip ^= 1
 
+        if re.search(r"Alg", line):
+            line = re.sub(r"(\d+\. *)?(Alg\w*)( *)(.*$)", r"\2 \4", line)
+            self.can_strip ^= 1
 
-def adjust_question(line):
-    global can_strip
-    # arrange the line, so each answer is on a new line
-    if re.search(r"[A-D]\.", line):
-        line = re.sub(r"([A-D]\.)( )?", r"\n\1\n", line)
-    # we want to have clearly the algorithm separated between the Algorithm and EndAlg
-    if re.search(r"Alg", line):
-        line = re.sub(r"(\d+\. *)?(Alg\w*)( *)(.*$)", r"\2\n\n\4", line)
-        can_strip ^= 1
+        line = line.rstrip()
 
-    if re.search(r"EndAlg", line):
-        line = re.sub(r"(\d+\. *)?(EndAlg\w*)( *)(.*$)", r"\1\n\n\3", line)
-        can_strip ^= 1
+        line = line.lstrip() if self.can_strip else line
 
-    line = line.rstrip()
+        return line
 
-    line = line.lstrip() if can_strip else line
+    def parse_question(self, text):
+        parsed_dict = {"question": "", "answers": [], "code": ""}
 
-    return line
+        # extract the answers from the question
+        answers_match = re.search(r"([A-D]\..*)", text, re.DOTALL)
+        answers_text = answers_match.group(1).strip() if answers_match else ""
 
+        # remove the answers section from the text
+        text = text.replace(answers_text, "")
 
-def parse_question(text):
-    parsed_dict = {"question": "", "answers": [], "code": ""}
+        # extract the code from the question
+        code_match = re.search(r"((Alg\w*)(.*?)(EndAlg\w*))", text, re.DOTALL)
+        code_text = code_match.group(1).strip() if code_match else ""
 
-    # extract the answers from the question
-    answers_match = re.search(r"([A-D]\..*)", text, re.DOTALL)
-    answers_text = answers_match.group(1).strip() if answers_match else ""
+        # remove the code section from the text
+        text = text.replace(code_text, "")
 
-    # remove the answers section from the text
-    text = text.replace(answers_text, "")
+        # remove all the empty lines from the text
+        text = re.sub(r"\n+", "\n", text)
 
-    # extract the code from the question
-    code_match = re.search(r"((Alg\w*)(.*?)(EndAlg\w*))", text, re.DOTALL)
-    code_text = code_match.group(1).strip() if code_match else ""
+        # now, all the remaining text is the question, so it can be on a single line
+        text = text.replace("\n", " ")
 
-    # remove the code section from the text
-    text = text.replace(code_text, "")
+        # remove the question number
+        text = re.sub(r"^\d+\. ?", "", text)
 
-    # remove all the empty lines from the text
-    text = re.sub(r"\n+", "\n", text)
+        parsed_dict["question"] = text
+        parsed_dict["code"] = code_text
 
-    # now, all the remaining text is the question, so it can be on a single line
-    text = text.replace("\n", " ")
+        # parsing answers
+        answers_list = re.findall(
+            r"([A-D])\.(.*?)(?=(?:[A-D]\.|$))", answers_text, re.DOTALL)
+        parsed_dict["answers"] = (
+            [{key: value.strip()}
+             for key, value in answers_list] if answers_list else []
+        )
 
-    # remove the question number
-    text = re.sub(r"^\d+\. ?", "", text)
+        return parsed_dict
 
-    parsed_dict["question"] = text
-    parsed_dict["code"] = code_text
+    def parse_pdf(self, filename, pgs=40):
+        # pgs -> how many pages to parse
 
-    # parsing answers
-    answers_list = re.findall(
-        r"([A-Z])\.(.*?)(?=(?:[A-Z]\.|$))", answers_text, re.DOTALL
-    )
-    parsed_dict["answers"] = (
-        [{key: value.strip()} for key, value in answers_list] if answers_list else []
-    )
+        reader = PdfReader(filename)
 
-    return parsed_dict
+        text = ""
 
+        for i in range(min(len(reader.pages), pgs)):
+            page = reader.pages[i]
 
-# converts a pdf into raw text
-def parse_pdf(filename, pgs=40):
-    # pgs -> how many pages to parse
+            page_text = page.extract_text()
+            text += page_text
 
-    reader = PdfReader(filename)
+        return text
 
-    text = ""
+    def parse_text(self, text):
+        lines = text.split("\n")
 
-    for i in range(min(len(reader.pages), pgs)):
-        page = reader.pages[i]
+        questions = []
+        current_question = []
+        correct_answers = []
 
-        page_text = page.extract_text()
-        text += page_text
+        # 0 - questions, 1 - answers
+        category = 1
 
-    return text
+        for line in lines:
+            # if we find the word "UNIVERSITA", we skip (romanian version)
+            if re.search(r"UNIVERSITA", line.strip()):
+                break
 
+            if re.search(r"BAB", line.strip()):
+                category ^= 1
 
-# converts raw text into an array of questions
-def parse_text(text):
-    lines = text.split("\n")
+            if category:
+                line = self.adjust_answer(line)
 
-    questions = []
-    current_question = []
-    correct_answers = []
-
-    # 0 - questions, 1 - answers
-    category = 1
-
-    for line in lines:
-        if re.match(r"BAB", line.strip()):
-            category ^= 1
-
-        if category:
-            line = adjust_answer(line)
-
-            if re.match(r"^[A-D]+$", line):
-                correct_answers.append(line)
-        else:
-            line = adjust_question(line)
-            # if the line starts with a digit followed by a period, it's the start of a new question
-            if re.match(r" *\d+\. \w", line):
-                if current_question:
-                    questions.append("\n".join(current_question))
-                current_question = [line]
+                if re.match(r"^[A-D]+$", line):
+                    correct_answers.append(line)
             else:
-                current_question.append(line)
+                line = self.adjust_question(line)
+                # if the line starts with a digit followed by a period, it's the start of a new question
+                if re.match(r" *\d+\w?\. {1,3}\w", line):
+                    if current_question:
+                        questions.append("\n".join(current_question))
+                    current_question = [line]
+                else:
+                    current_question.append(line)
 
-    if current_question:
-        questions.append("\n".join(current_question))
+        if current_question:
+            questions.append("\n".join(current_question))
 
-    questions = questions[1:]
+        questions = questions[1:]
 
-    parsed_questions = []
+        parsed_questions = []
 
-    print(correct_answers)
+        for question in questions:
+            if question.strip():
+                questions_properties = self.parse_question(question)
+                # add the correct answers to the question
+                if correct_answers:
+                    questions_properties["correct_answers"] = correct_answers.pop(
+                        0)
+                questions_properties["index"] = len(parsed_questions) + 1
+                parsed_questions.append(questions_properties)
 
-    for question in questions:
-        if question.strip():
-            questions_properties = parse_question(question)
-            # add the correct answer to the question
-            # questions_properties["correct_answers"] = correct_answers.pop(0)
-            # add index to the question
-            questions_properties["index"] = len(parsed_questions) + 1
-            parsed_questions.append(questions_properties)
+        return parsed_questions
 
-    # print(parsed_questions)
-    return parsed_questions
-
-
-# converts a pdf-file into a json array of questions
-def pdf_to_json(filename, pgs=40):
-    text = parse_pdf(filename, pgs)
-    parsed_questions = parse_text(text)
-    parsed_questions = json.dumps(parsed_questions, indent=4, ensure_ascii=False)
-    return parsed_questions
+    def pdf_to_json(self, filename, pgs=40):
+        text = self.parse_pdf(filename, pgs)
+        parsed_questions = self.parse_text(text)
+        parsed_questions = json.dumps(
+            parsed_questions, indent=4, ensure_ascii=False)
+        return parsed_questions
 
 
+# Usage example:
 if __name__ == "__main__":
+    pdf_parser = PdfParser()
     filename = "2022.pdf"
-    parsed_questions = pdf_to_json(filename)
+    parsed_questions = pdf_parser.pdf_to_json(filename)
 
-    with open("questions.json", "w") as f:
-        f.write(parsed_questions)
+    # with open("questions.json", "w") as f:
+    #     f.write(parsed_questions)
--- scrape.py
+++ scrape.py
@@ -1,104 +1,91 @@
 import json
 import os
-import sys
 import requests
-from admito_parser import parse_text, parse_pdf
-
+import urllib.request
+import admito_parser
+from bs4 import BeautifulSoup
 from PyPDF2 import PdfMerger
 
 
-def merge_pdfs(_pdfs):
-    mergeFile = PdfMerger()
+class AdmitoParser:
+    def __init__(self):
+        self.admito_parser = admito_parser.PdfParser()
+        self.url = "https://www.cs.ubbcluj.ro/admitere/nivel-licenta/subiecte-din-anii-precedenti/"
+        self.subjects_texts = []
+        self.subject_folder_names = []
 
-    for i, _pdf in enumerate(_pdfs):
-        if i == 1:
-            mergeFile.append(_pdf, pages=(0, 1))
-        else:
-            mergeFile.append(_pdf)
+    def merge_pdfs(self, _pdfs):
+        mergeFile = PdfMerger()
 
-    mergeFile.write("file.pdf")
+        for i, _pdf in enumerate(_pdfs):
+            if i == 1:
+                mergeFile.append(_pdf, pages=(0, 1))
+            else:
+                mergeFile.append(_pdf)
+
+        mergeFile.write("file.pdf")
+
+    def download_and_parse(self):
+        req = requests.get(self.url)
+        soup = BeautifulSoup(req.text, "html.parser")
+
+        urls = []
+        for link in soup.find_all("a"):
+            url = link.get("href")
+
+            # we only care about multiple choice questions (2021 and beyond)
+            if (
+                any(str(year) in url for year in range(2021, 2025))
+                and url.endswith(".pdf")
+                and "Informatica" in url
+            ):
+                urls.append(url)
+
+        for i, url in enumerate(urls):
+            if "raspunsuri" in url or "solutii" in url:
+                # skip, we handled this in the previous iteration
+                continue
+
+            # if url contains word 'RO' replace it with 'EN'
+            if "RO" in url:
+                url = url.replace("RO", "EN")
+
+            # download current file
+            filename = url.split("/")[-1]
+            urllib.request.urlretrieve(url, filename)
+
+            self.subject_folder_names.append(filename)
+
+            # check if it has a pair of answers
+            if "raspunsuri" in urls[i + 1] or "solutii" in urls[i + 1]:
+                # combine the two pdfs
+                filename2 = urls[i + 1].split("/")[-1]
+                urllib.request.urlretrieve(urls[i + 1], filename2)
+
+                self.merge_pdfs([filename, filename2])
+
+                text = self.admito_parser.parse_pdf("file.pdf")
+                parsed_questions = self.admito_parser.parse_text(text)
+                self.subjects_texts.append(parsed_questions)
+
+                # os.remove("file.pdf")
+            else:
+                text = self.admito_parser.parse_pdf(filename)
+                self.subjects_texts.append(text)
+
+            # delete the file
+            os.remove(filename)
+
+        self.subjects_texts = json.dumps(
+            self.subjects_texts, indent=4, ensure_ascii=False)
+
+    def save_to_json_file(self):
+        with open("questions.json", "w") as f:
+            f.write(self.subjects_texts)
 
 
-from bs4 import BeautifulSoup
-import urllib.request
-
-url = "https://www.cs.ubbcluj.ro/admitere/nivel-licenta/subiecte-din-anii-precedenti/"
-
-
+# Usage example:
 if __name__ == "__main__":
-    # merge_pdfs(
-    #     [
-    #         "Informatica%20iulie%202022%20EN.pdf",
-    #         "Informatica%20raspunsuri%20%28toate%20limbile%29%20iulie%202022.pdf",
-    #     ]
-    # )
-
-    req = requests.get(url)
-    soup = BeautifulSoup(req.text, "html.parser")
-
-    urls = []
-    for link in soup.find_all("a"):
-        url = link.get("href")
-
-        # we only care about multiple choice questions (2021 and beyond)
-        if (
-            any(str(year) in url for year in range(2021, 2025))
-            and url.endswith(".pdf")
-            and "Informatica" in url
-        ):
-            urls.append(url)
-
-    subjects_texts = []
-
-    subject_folder_names = []
-
-    # for i, url in enumerate(urls):
-    #     print(i, url)
-
-    for i, url in enumerate(urls):
-        if "raspunsuri" in url or "solutii" in url:
-            # skip, we handled this in the previous iteration
-            continue
-
-        # if url contains word 'RO' replace it with 'EN'
-        if "RO" in url:
-            url = url.replace("RO", "EN")
-
-        # download current file
-        filename = url.split("/")[-1]
-        urllib.request.urlretrieve(url, filename)
-
-        subject_folder_names.append(filename)
-
-        # check if it has a pair or answers
-        if "raspunsuri" in urls[i + 1] or "solutii" in urls[i + 1]:
-            # combine the two pdfs
-            filename2 = urls[i + 1].split("/")[-1]
-            urllib.request.urlretrieve(urls[i + 1], filename2)
-
-            merge_pdfs([filename, filename2])
-
-            text = parse_pdf("file.pdf")
-            parsed_questions = parse_text(text)
-            subjects_texts.append(parsed_questions)
-
-            os.remove("file.pdf")
-
-        else:
-            text = parse_pdf(filename)
-            subjects_texts.append(text)
-
-        # delete the file
-        os.remove(filename)
-
-    subjects_texts = json.dumps(subjects_texts, indent=4, ensure_ascii=False)
-
-    # for each question, make a folder with that name, and inside create a json file with the question
-    # for i, subject in enumerate(subjects_texts):
-    #     subject_folder_name = subject_folder_names[i].split(".")[0]
-    #     os.mkdir(subject_folder_name)
-    #     with open(f"{subject_folder_name}/questions.json", "w") as f:
-    #         f.write(subject)
-
-    with open("questions.json", "w") as f:
-        f.write(subjects_texts)
+    admito_parser = AdmitoParser()
+    admito_parser.download_and_parse()
+    admito_parser.save_to_json_file()
